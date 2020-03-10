8d8a165b5abbb01f1a57161416e145b8511402a6
Eunice Elizabeth Dhivya <eunicedhivya@thefederal.in>
2020-03-10 18:50:48 +0530

    Find center and add circle to countries

--- js/map.js
+++ js/map.js
@@ -24,16 +24,29 @@ function drawWorldMap(selector){
 
         var countrywise = topojson.feature(mapboundary, mapboundary.objects.countries).features;
 
-        console.log(countrywise);
+        // var countryCentroid = centroids(countrywise);
 
-        g.selectAll(".constituency")
+        // console.log(countryCentroid);
+
+        g.selectAll(".country")
             .data(countrywise).enter().append("path")
                 .attr("d", geoPath)
                 .attr("class", "country")
                 .attr('fill', "#dcdcdc")
             .append("title")
                 .text(function(d,i){
-                    return "Hello World!"
+
+                    var fd = _.filter(testData, function(obj){
+                        return obj.countryId === parseInt(d.id);
+                    })
+                    console.log(fd[0]);
+
+                    if(fd[0] !== undefined){
+                        return d.id + " " + fd[0]["Country"];
+                    }else{
+                        return d.id;
+                    }
+                    
                 })
         
         svg.call(d3.zoom()
@@ -42,6 +55,17 @@ function drawWorldMap(selector){
             .on("zoom", function(){
                 g.attr("transform", d3.event.transform);
             }));
+
+        g.selectAll(".country-center")
+            .data(countrywise).enter().append("circle")
+            .attr("class", "country-center")
+            .attr("cx", function(d){
+                return projection(d3.geoCentroid(d))[0] 
+            })
+            .attr("cy", function(d){
+                return projection(d3.geoCentroid(d))[1] 
+            })
+            .attr("r", 1)
           
         
 
