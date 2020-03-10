015e5f81b326c1bca0ccbc8395673df8342a72e5
Eunice Elizabeth Dhivya <eunicedhivya@thefederal.in>
2020-03-10 16:11:08 +0530

    clear duplicate comments & add zoom effect

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 function drawWorldMap(selector){
+
     var width = 350, height = 250, scale = 55, center = [10, 45];
 
     var source = "https://unpkg.com/visionscarto-world-atlas@0.0.4/world/50m.json";
@@ -8,12 +9,8 @@ function drawWorldMap(selector){
     .attr("class", "worldmap")
     .attr("viewBox", "0 0 " + width + " " + height)
     .attr("preserveAspectRatio", "xMinYMin")
-    .append("g")
-
-    // var projection = d3.geoMercator()
-    // .scale(scale)
-    // .center(center)
-    // .translate([width / 2, height / 2])
+    
+    var g = svg.append("g")
     
     var projection = d3.geoMercator()
     .scale(scale)
@@ -29,13 +26,25 @@ function drawWorldMap(selector){
 
         // console.log(countrywise);
 
-        svg.selectAll(".constituency")
+        g.selectAll(".constituency")
             .data(countrywise).enter().append("path")
-            .attr("d", geoPath)
-            .attr("class", "country")
-            .attr('fill', "green")
-            .attr('stroke-opacity', "1")
-
+                .attr("d", geoPath)
+                .attr("class", "country")
+                .attr('fill', "green")
+                .attr('stroke-opacity', "1")
+            .append("title")
+                .text(function(d,i){
+                    console.log(d.id);
+                    return "Hello World!"
+                })
+        
+        svg.call(d3.zoom()
+            .extent([[0, 0], [width, height]])
+            .scaleExtent([1, 8])
+            .on("zoom", function(){
+                g.attr("transform", d3.event.transform);
+            }));
+          
         
 
     });
