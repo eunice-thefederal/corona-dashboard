5253549c337604e5437b71bddd44d83004a6497b
Marimuthu <31431414+marimuthu-php@users.noreply.github.com>
2020-03-13 13:20:02 +0530

    New file loadData.js

---
+++ js/loadData.js
@@ -0,0 +1,74 @@
+var countrywisedata = (function() {
+    var countrywisedata = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'dataType': 'json',
+        'url': 'https://thefederal.com/api/scraper.php?m=Corona&t=totalCases',
+        'success': function(data) {
+            countrywisedata = data["records"];
+              // console.log("JSON RECORDS DATA:"+JSON.stringify(data["records"], null, 4));
+            //console.log("DATA:"+data);
+        }
+    });
+    return countrywisedata;
+})();
+var dailyDeathData = (function() {
+    var dailyDeathData = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'dataType': 'json',
+        'url': 'https://thefederal.com/api/scraper.php?m=Corona&t=dailyDeaths',
+        'success': function(data) {
+            dailyDeathData = data;
+            console.log("JSON RECORDS DAILY DEATH DATA:"+JSON.stringify(data, null, 4));
+        }
+    });
+    return dailyDeathData;
+})();
+var dailyTotalDeathData = (function() {
+    var dailyTotalDeathData = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'dataType': 'json',
+        'url': 'https://thefederal.com/api/scraper.php?m=Corona&t=totalDeaths',
+        'success': function(data) {
+            dailyTotalDeathData = data;
+             //  console.log("JSON RECORDS DATA:"+JSON.stringify(data["records"], null, 4));
+            //console.log("DATA:"+data);
+        }
+    });
+    return dailyTotalDeathData;
+})();
+var globalTotalData = (function() {
+    var globalTotalData = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'dataType': 'json',
+        'url': 'https://thefederal.com/api/scraper.php?m=Corona&t=totalCases',
+        'success': function(data) {
+            globalTotalData = data["totals"];
+
+        }
+    });
+    return globalTotalData;
+})();
+var currentData = (function() {
+    var currentData = null;
+    jQuery.ajax({
+        'async': false,
+        'global': false,
+        'dataType': 'json',
+        'url': 'https://thefederal.com/api/scraper.php?m=Corona&t=chinaCases',
+        'success': function(data) {
+            currentData = data;
+
+        }
+    });
+    return currentData;
+})();
+
+
